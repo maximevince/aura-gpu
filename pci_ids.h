4f6280744e01f0be4b2c2be3e764c338904e4ec8
Adam Honse <calcprogrammer1@gmail.com>
2020-07-14 15:04:57 -0500

    Add PCI ID for Sapphire RX580 Nitro+

--- pci_ids.h
+++ pci_ids.h
@@ -10,6 +10,7 @@
     NVIDIA devices already have the adapter loaded.
  */
 static const struct pci_device_id pciidlist[] = {
+    {0x1002, 0x67df, 0x1da2, 0xe366, 0, 0, CHIP_POLARIS10},     // RX580 (Sapphire Nitro+)
     {0x1002, 0x67df, 0x1043, 0x0517, 0, 0, CHIP_POLARIS10},     // RX580 (Strix)
     {0x1002, 0x687F, 0x1043, 0x0555, 0, 0, CHIP_VEGA10},        // Vega 56 (Strix)
     {0x1002, 0x687F, 0x1043, 0x04c4, 0, 0, CHIP_VEGA10},        // Vega 64 (Strix)
