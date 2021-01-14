6fa891ff8172357067a7884553716de5189d894b
Tomasz Torcz <tomek@pipebreaker.pl>
2021-01-14 14:35:32 +0100

    add PCI IDS for Radeon RX560
    Tested with Asus Strix RX 560 4GB

--- pci_ids.h
+++ pci_ids.h
@@ -13,6 +13,7 @@ static const struct pci_device_id pciidlist[] = {
     {0x1002, 0x67df, 0x1da2, 0xe366, 0, 0, CHIP_POLARIS10},     // RX580 (Sapphire Nitro+)
     {0x1002, 0x67df, 0x1043, 0x04fd, 0, 0, CHIP_POLARIS10},     // RX480 (Strix)
     {0x1002, 0x67df, 0x1043, 0x0517, 0, 0, CHIP_POLARIS10},     // RX580 (Strix)
+    {0x1002, 0x67FF, 0x1043, 0x04BC, 0, 0, CHIP_POLARIS11},     // RX560
     {0x1002, 0x687F, 0x1043, 0x0555, 0, 0, CHIP_VEGA10},        // Vega 56 (Strix)
     {0x1002, 0x687F, 0x1043, 0x04c4, 0, 0, CHIP_VEGA10},        // Vega 64 (Strix)
     // {0x1002, 0x731f, 0x1043, 0x04e2, 0, 0, CHIP_NAVI10},     // RX5700XT (Strix)
